ca58ff852219b3dee27652a3a8fb678c20ada5cb
Alla Katkova <allakatkova@bk.ru>
2022-12-01 00:04:33 +0500

    Added user reading from VK

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pprint import pprint
+import requests
 
 DIR_BACKUP_YD = '/backup_VK_photo/'
 TOKENS_FILE = 'tokens.txt'
+URL_VK = 'https://api.vk.com/method/'
 
 
 def get_tokens(tokens_file):
@@ -13,12 +15,25 @@ def get_tokens(tokens_file):
 
 
 def get_userid_vk():
-    # раскомментировать в готовой программе
+    # раскомментировать в готовой программе      !!!!!!!!!!!!!!!!!!!!!
     # user_id = input('Введите идентификатор (ID) пользователя: ')
     user_id = '24669426'
     return user_id
 
 
+def get_info_vk(userid, token):
+    url = URL_VK + 'users.get'
+    params = {
+        'user_ids': userid,
+        'access_token': token,
+        'v': '5.131'
+    }
+    res = requests.get(url, params=params)
+    return res.json()
+
+
 if __name__ == '__main__':
     userid_vk = get_userid_vk()
     tokens_list = get_tokens(TOKENS_FILE)
+    info = get_info_vk(userid_vk, tokens_list['VK'])
+    pprint(info)
