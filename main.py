9fbfe6d025ed311233401e85adad206cc7d9105a
Alla Katkova <allakatkova@bk.ru>
2022-12-13 21:03:19 +0500

    Implemented saving of files

--- main.py
+++ main.py
@@ -6,6 +6,80 @@ TOKENS_FILE = 'tokens.txt'
 URL_VK = 'https://api.vk.com/method/'
 
 
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        url_files_list = 'https://cloud-api.yandex.net:443/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(url_files_list, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path, link):
+        url_upload = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "url": link, "disable_redirects": "false"}
+        response = requests.post(url_upload, headers=headers, params=params)
+        return response
+
+    def upload(self, folder_path, list_files):
+        count_file = 0
+        for filename, url_file in list_files.items():
+            disk_file_path = folder_path + filename
+            href_upload = self._get_upload_link(disk_file_path=disk_file_path, link=url_file)
+            href_upload.raise_for_status()
+            count_file += 1
+            if href_upload.status_code == 202:
+                pprint(f'Сохранена {count_file} фотография из {len(list_files)} под именем {filename}')
+        print('Выполнено! Бэкап фотографий создан успешно.')
+
+
+class VkDownloader:
+    def __init__(self, token: str, userid: str):
+        self.token = token
+        self.userid = userid
+
+    def get_info_photos_album(self, count_photo=5):
+        url = URL_VK + 'photos.get'
+        params = {
+            'owner_id': self.userid,
+            'album_id': 'profile',
+            'extended': '1',
+            'count': count_photo,
+            'access_token': self.token,
+            'v': '5.131'
+        }
+        res = requests.get(url, params=params)
+        return res.json()
+
+    def get_photos_data(self, info_photos_album):
+        list_file_for_backup = {}
+        list_photo_data = info_photos_album['response']['items']
+        for photo in list_photo_data:
+            max_size = 0
+            url_max_size = ''
+            # определить фото в максимальном качестве разрешения
+            for size in photo['sizes']:
+                if size['height'] >= max_size:
+                    max_size = size['height']
+                    url_max_size = size['url']
+            file_name = str(photo['likes']['count'])
+            date_photo = photo['date']
+            # если количество лайков одинаково, то добавить дату загрузки.
+            if list_file_for_backup.get(file_name) is not None:
+                file_name = f"{file_name}_{date_photo}"
+            file_name += '.jpg'
+            list_file_for_backup[file_name] = url_max_size
+        return list_file_for_backup
+
+
 def get_tokens(tokens_file):
     tokens_from_file = {}
     with open(tokens_file, 'rt', encoding='utf-8') as file:
@@ -14,29 +88,18 @@ def get_tokens(tokens_file):
     return tokens_from_file
 
 
-def get_userid_vk():
-    # раскомментировать в готовой программе      !!!!!!!!!!!!!!!!!!!!!
-    # user_id = input('Введите идентификатор (ID) пользователя: ')
-    user_id = '24669426'
-    return user_id
-
-
-def get_list_photos_album(userid, token, count_photo=5):
-    url = URL_VK + 'photos.get'
-    params = {
-        'owner_id': userid,
-        'album_id': 'profile',
-        'extended': '1',
-        'count': count_photo,
-        'access_token': token,
-        'v': '5.131'
-    }
-    res = requests.get(url, params=params)
-    return res.json()
-
-
 if __name__ == '__main__':
-    userid_vk = get_userid_vk()
+
+    # userid_vk = input('Введите ID пользователя ВКонтакте: ')
+    userid_vk = '24669426'
+
     tokens_list = get_tokens(TOKENS_FILE)
-    info = get_list_photos_album(userid_vk, tokens_list['VK'])
-    pprint(info)
+    # token_yd = input('Введите token Я.Диск: ')
+    token_yd = tokens_list['YD']
+
+    downloader_from_vk = VkDownloader(tokens_list['VK'], userid_vk)
+    info_photos_album = downloader_from_vk.get_info_photos_album()
+    names_files_with_urls = downloader_from_vk.get_photos_data(info_photos_album)
+
+    uploader = YaUploader(token_yd)
+    uploader.upload(DIR_BACKUP_YD, names_files_with_urls)
